2faf4146d0a01c3b84655e71e66f3eac88922750
Caio Rodrigo <caioxyz@hotmail.com>
2019-02-04 17:39:35 -0300

    ALU update

--- rtl/modules/ALU/ALU.v
+++ rtl/modules/ALU/ALU.v
@@ -1,27 +1,5 @@
 /*
 	ALU.v
-
-	Várias instruções dependem na verdade de apenas poucas operações.
-	Operações implementadas:
-		- ADD
-		- SUB (ADD com entrada invertida)
-		- SLL
-		- SLT
-		- SLUT
-		- XOR
-		- SRL (Mesmo OP de SRA mas se difere no bit30)
-		- SRA
-		- OR
-		- AND
-
-	ADD e SUB possuem o mesmo op (funct3 no manual do RISC-V) e são desambiguadas
-	pelo sinal do bit 30 da instrução. O mesmo ocorre com SRL e SRA.
-	Formato adotado de op: {bit30, fucnt3}
-
-	Implementações de ALU no git:
-	https://github.com/cliffordwolf/picorv32/blob/master/picorv32.v
-	https://github.com/onchipuis/mriscvcore/blob/1e94fa95bcdddb889be205ed661b9addff1221a4/ALU/ALU.v
-
 */
 `timescale 1ns / 1ps
 
@@ -34,7 +12,7 @@ module ALU(
 	input  [`XLEN-1:0] i_Rb,
 
 	output o_Z, // Z Flag
-	output reg [`XLEN-1:0] o_Rc  // Verificar se deixa o carry atrelado a Rc ou o coloca em um sinal separado
+	output reg [`XLEN-1:0] o_Rc 
 );
 
 	always@(*) begin
@@ -42,7 +20,7 @@ module ALU(
 			`ALU_ADD:  o_Rc = i_Ra + i_Rb;
 			`ALU_SUB:  o_Rc = i_Ra - i_Rb;
 			`ALU_SLL:  o_Rc = i_Ra << i_Rb[4:0];
-			`ALU_SLT:  o_Rc = {{`XLEN-1{1'b0}}, $signed(i_Ra) < $signed(i_Rb)}; //($signed(i_Ra) < $signed(i_Rb)) ?
+			`ALU_SLT:  o_Rc = {{`XLEN-1{1'b0}}, $signed(i_Ra) < $signed(i_Rb)}; 
 			`ALU_SLTU: o_Rc = {{`XLEN-1{1'b0}}, i_Ra < i_Rb};
 			`ALU_XOR:  o_Rc = i_Ra ^ i_Rb;
 			`ALU_SRL:  o_Rc = i_Ra >>  i_Rb[4:0];
